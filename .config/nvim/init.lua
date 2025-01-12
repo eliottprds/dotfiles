f27101185744675c980616a4ad80a7b5de7d4785
eliottprds <eliottparadis@icloud.com>
2025-01-12 16:48:49 +0100

    added some wallpaper

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,4 +1,6 @@
 -- Basic configuration:
+
+vim.opt.guifont = "Hack Nerd Font:h16"
 vim.opt.number = true          -- Affiche les numéros de ligne
 vim.opt.relativenumber = false  -- Affiche les numéros relatifs (utile pour les déplacements)
 vim.opt.tabstop = 4            -- Taille d'une tabulation
@@ -14,33 +16,57 @@ vim.opt.clipboard = "unnamedplus" -- Partage le presse-papier avec le système
 vim.opt.rtp:prepend("~/.local/share/nvim/lazy/lazy.nvim")
 
 -- Plugins: 
+
 require("lazy").setup({
+    {
+  "iamcco/markdown-preview.nvim",
+  cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+  build = "cd app && yarn install",
+  init = function()
+    vim.g.mkdp_filetypes = { "markdown" }
+  end,
+  ft = { "markdown" },
+},
     { "nvim-tree/nvim-tree.lua"},
     { "nvim-tree/nvim-web-devicons"},
     { "akinsho/toggleterm.nvim", version = "*", config = true },
-    { "akinsho/toggleterm.nvim", version = "*", config = true},	
     { "neovim/nvim-lspconfig"},
     { "nvim-lualine/lualine.nvim" },
-    { "catppuccin/nvim", name = "catppuccin", priority = 1000 }
+    { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+    { "nvim-telescope/telescope.nvim", dependencies = { "nvim-lua/plenary.nvim" }},
+    { "hrsh7th/nvim-cmp" },                -- Plugin principal
+    { "hrsh7th/cmp-nvim-lsp" },            -- Source pour LSP
+    { "hrsh7th/cmp-buffer" },              -- Source pour les buffers ouverts
+    { "hrsh7th/cmp-path" },                -- Source pour les chemins de fichiers
+    { "saadparwaiz1/cmp_luasnip" },        -- Source pour les snippets
+    { "L3MON4D3/LuaSnip" },
+    { "akinsho/bufferline.nvim", dependencies = "nvim-tree/nvim-web-devicons" },
+    { "Pocco81/auto-save.nvim"},
+    { "windwp/nvim-autopairs" }
+
 })
-
-
 
 
 -- Theme:
 vim.cmd.colorscheme("catppuccin-mocha")
-
-
--- Plugins conf
-
-require("toggleterm").setup {
-    open_mapping = [[<C-t>]], -- Ouvre le terminal avec Ctrl + t
-    direction = "horizontal",      -- Terminal flottant (peut aussi être "horizontal" ou "vertical")
-}
 require("lualine").setup {
     options = { theme = "catppuccin" }
 }
 
+-- Plugins conf
+--
+-- auto-save
+require("auto-save").setup {}
+--auto-pair:
+require("nvim-autopairs").setup {}
+
+-- terminal
+require("toggleterm").setup {
+    open_mapping = [[<C-t>]], -- Ouvre le terminal avec Ctrl + t
+    direction = "horizontal",      -- Terminal flottant (peut aussi être "horizontal" ou "vertical")
+}
+
+--tree conf
 require("nvim-tree").setup {
     view = {
         width = 30,               -- Largeur de la fenêtre de l'arbre
@@ -62,7 +88,36 @@ require("nvim-tree").setup {
         ignore = false,
     }
 }
+
 vim.api.nvim_set_keymap('n', '<C-n>', ':NvimTreeToggle<CR>', { noremap = true, silent = true })
+
+--bufferline
+require("bufferline").setup {}
+vim.api.nvim_set_keymap('n', '<Tab>', ':BufferLineCycleNext<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<S-Tab>', ':BufferLineCyclePrev<CR>',  { noremap = true, silent = true })
+
+--telescope conf
+require("telescope").setup {}
+vim.api.nvim_set_keymap('n', '<C-p>', ':Telescope find_files<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-f>', ':Telescope live_grep<CR>', { noremap = true, silent = true })
+
+-- CMP conf
+
+local cmp = require("cmp")
+
+cmp.setup({
+    mapping = {
+        ['<Tab>'] = cmp.mapping.select_next_item(),
+        ['<S-Tab>'] = cmp.mapping.select_prev_item(),
+        ['<CR>'] = cmp.mapping.confirm({ select = true }),
+    },
+    sources = {
+        { name = "nvim_lsp" },
+        { name = "buffer" },
+        { name = "path" }
+    }
+})
+
 
 -- LSP conf
 local lspconfig = require("lspconfig")
@@ -70,11 +125,12 @@ local lspconfig = require("lspconfig")
 lspconfig.lua_ls.setup {
     settings = {
         Lua = {
-            runtime = { version = "LuaJIT"},
-            diagnostics = { globals = {"vim" } }
+            runtime = { version = "LuaJIT" },    -- Utiliser LuaJIT (version de Lua utilisée par Neovim)
+            diagnostics = { globals = { "vim" } } -- Ne pas signaler d'erreur pour les variables globales comme `vim`
         }
     }
 }
+
 lspconfig.dockerls.setup {}
 lspconfig.pyright.setup {}
 lspconfig.gopls.setup {}
